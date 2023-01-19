a8703f7702bc795b05e9d1d59186374fb307be64
dsjordao <jordaodaniel@hotmail.com>
2023-01-19 09:24:31 -0300

    Alterando BD de 100 para 500

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -11,7 +11,7 @@ import { map, tap } from 'rxjs/operators';
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500';
   constructor(
     private http: HttpClient
   ) { }
